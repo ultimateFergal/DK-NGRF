c1fa6dae05c8abf3b23fbf83ccdd7e7ac88a9f4c
Luis Fernando Galarcio Marin <luis.galarcio@arkixsa.com>
2018-10-14 18:37:02 -0500

    form builder, validators

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +9,39 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-  customer = new Customer();
+  customerForm: FormGroup;
+  customer: Customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      // firstName: {value: 'n/a', disabled: true}, // Otra forma de definir valor inicial en formbuilder
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email/* Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.%+]') */]],
+      sendCatalog: {value: true, disabled: true},    
+    });
+
+/*     this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    }); */
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void{
+    this.customerForm.setValue({ // Setvalue es para todos los campos, pathvalue sólo para unos cuantos
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@torchwood.com',
+      sendCatalog: false,
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm));
   }
 }
